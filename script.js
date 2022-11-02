d2f2114b9f39ae99c80ebc98479d1dcba56c4bd0
Ighor Melo <ighor-melo@hotmail.com>
2022-11-02 16:57:10 -0300

    feat: create work section

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-const navMenu = document.getElementById('nav-menu')
-const navToggle = document.getElementById('nav-toggle')
-const navClose = document.getElementById('nav-close')
-
-if (navToggle) {
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu')
-    })
+const experience = {
+    companys: [
+        {
+            name: 'UolEdTech',
+            role: 'Analista de Desenvolvimento Junior',
+            start_date: 1633125600,
+            end_date: null,
+            description: 'Responsável pela implementação de novas funcionalidades e telas na plataforma de educação corporativa da empresa utilizando testes automatizados. Essas funcionalidades deveriam ser implementadas pixel perfect e de maneira responsiva.',
+            technologies: ['VueJs', 'Javascript', 'Git', 'Jest'],
+            icon: '',
+        },
+        {
+            name: 'Skore',
+            role: 'Estagiário',
+            start_date: 1575324000,
+            end_date: 1633039200,
+            description: 'Responsável pela resolução de problemas no Front-end e Back-end em uma plataforma de educação corporativa.',
+            technologies: ['Ruby', 'Javascript', 'VueJs', 'Git'],
+            icon: '',
+        }
+    ]
 }
-if (navClose) {
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
-    })
-}
 
-const navLink = document.querySelectorAll('.nav_link')
-console.log(navLink)
-function linkAction() {
-    navMenu.classList.remove('show-menu')
-}
+const menu = document.querySelector('.work-menu')
+experience.companys.forEach(company => {
+    const work = document.createElement('li')
+    const text = document.createTextNode('ola')
+    work.innerText = company.name
+    menu.appendChild(work)
+})
 
-navLink.forEach(n => n.addEventListener('click', linkAction))
