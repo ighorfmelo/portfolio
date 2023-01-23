3bae719ee1df196a319430a522000873273792ed
Ighor Melo <ighor-melo@hotmail.com>
2023-01-23 11:06:32 -0300

    feat(work-section): add script to show work description

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const experience = {
     companys: [
         {
-            name: 'UolEdTech',
+            name: 'UOL EdTech',
             role: 'Analista de Desenvolvimento Junior',
-            start_date: 1633125600,
+            start_date: '10/2021',
             end_date: null,
             description: 'Responsável pela implementação de novas funcionalidades e telas na plataforma de educação corporativa da empresa utilizando testes automatizados. Essas funcionalidades deveriam ser implementadas pixel perfect e de maneira responsiva.',
             technologies: ['VueJs', 'Javascript', 'Git', 'Jest'],
@@ -12,8 +12,8 @@ const experience = {
         {
             name: 'Skore',
             role: 'Estagiário',
-            start_date: 1575324000,
-            end_date: 1633039200,
+            start_date: '12/2019',
+            end_date: '09/2021',
             description: 'Responsável pela resolução de problemas no Front-end e Back-end em uma plataforma de educação corporativa.',
             technologies: ['Ruby', 'Javascript', 'VueJs', 'Git'],
             icon: '',
@@ -22,10 +22,39 @@ const experience = {
 }
 
 const menu = document.querySelector('.work-menu')
+const workDescription = document.querySelector('.work-description')
 experience.companys.forEach(company => {
     const work = document.createElement('li')
-    const text = document.createTextNode('ola')
+    manageWorks({ target: { innerText: 'UOL EdTech'}})
     work.innerText = company.name
     menu.appendChild(work)
 })
 
+menu.addEventListener('click', manageWorks)
+
+function manageWorks(event) {
+    selectButton(event.target.innerText)
+    const company = experience.companys.find(company => company.name == event.target.innerText)
+    const name = document.querySelector('.company-name')
+    const role = document.querySelector('.company-role')
+    const description = document.querySelector('.company-description')
+    const technologies = document.querySelector('.company-technologies')
+    const interval = document.querySelector('.company-interval')
+    name.innerText = company.name
+    role.innerText = company.role
+    description.innerText = company.description
+    technologies.innerText = company.technologies.join(' / ')
+    interval.innerText = company.end_date ? company.start_date + ' - ' + company.end_date : company.start_date + ' - Present'
+}
+
+function selectButton(text) {
+    menu.childNodes.forEach(element => {
+
+        if (element.innerText == text) {
+            element.classList.add("disabled");
+        } else {
+            element.classList.remove("disabled")
+        }
+
+    })
+}
